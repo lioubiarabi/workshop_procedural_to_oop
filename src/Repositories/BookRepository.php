4124ff78a07375129bd8259e98a4b79485cb4268
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-08 11:53:12 +0100

    create method to get all the books in mysql

--- src/Repositories/BookRepository.php
+++ src/Repositories/BookRepository.php
@@ -1,10 +1,18 @@
 <?php
-require '../Services/BookRepository.php';
 
 class BookRepository
 {
     private PDO $pdo;
     public function __construct(PDO $pdo) {
         $this->pdo = Database::connect();
+    }
+
+    public function getBooks() {
+        $result = [];
+        $stmt = $this->pdo->prepare("SELECT * from books");
+        $stmt->execute();
+        forEach($stmt->fetchAll() as $book){
+
+        }
     }
 }
