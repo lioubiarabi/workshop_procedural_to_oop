d969df17d2a12deb35d2f3119b7d84297f51dafe
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-08 12:01:41 +0100

    get all the books with their authors

--- src/Repositories/BookRepository.php
+++ src/Repositories/BookRepository.php
@@ -3,16 +3,25 @@
 class BookRepository
 {
     private PDO $pdo;
-    public function __construct(PDO $pdo) {
+    public function __construct(PDO $pdo)
+    {
         $this->pdo = Database::connect();
     }
 
-    public function getBooks() {
+    public function getBooks()
+    {
         $result = [];
-        $stmt = $this->pdo->prepare("SELECT * from books");
+        $stmt = $this->pdo->prepare("SELECT * FROM books INNER JOIN author where author.id=authorId");
         $stmt->execute();
-        forEach($stmt->fetchAll() as $book){
-
+        foreach ($stmt->fetchAll() as $book) {
+            $result[$book['title']] =
+                new Book(
+                    $book['id'],
+                    $book['title'],
+                    new Author($book['authorId'], $book['name']),
+                    $book['price'],
+                    $book['stock']
+                );
         }
     }
 }
