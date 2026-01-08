fbee3794d82e79d978e3597dda7c3a2704bec03b
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-08 11:29:13 +0100

    create books table in mySQL database

--- src/Core/db.sql
+++ src/Core/db.sql
@@ -2,6 +2,16 @@ CREATE DATABASE workshop1;
 use workshop1;
 
 CREATE Table author (
-    id AUTO_INCREMENT not NULL PRIMARY key,
+    id int AUTO_INCREMENT PRIMARY key,
     name VARCHAR(50) not NULL UNIQUE
+);
+
+CREATE Table books (
+    id int AUTO_INCREMENT PRIMARY key,
+    title VARCHAR(50) not NULL UNIQUE,
+    price int not null,
+    stock int not null,
+    authorId int not NULL,
+
+    Foreign Key (authorId) REFERENCES author(id)
 );
